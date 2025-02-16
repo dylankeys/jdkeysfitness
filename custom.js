da5d364489d1e1f881fb9dde0b22bb6ccf157ad4
Dylan Keys <dylankeys@btinternet.com>
2025-02-16 16:11:11 +0000

    Adding data to from JQuery

--- custom.js
+++ custom.js
@@ -4,7 +4,7 @@ $('#bookingModal').on('show.bs.modal', function (event) {
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
-    modal.find('.modal-body input.booking').val(booking)
+    modal.find('.modal-body input.booking').prop('value', booking)
   })
 
 var counter = 1;
