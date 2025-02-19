a894ca9daaa46516675ee401673f2b4f0ad6baf6
Dylan Keys <dylankeys@btinternet.com>
2025-02-19 18:03:45 +0000

    Testing SES code

--- email.php
+++ email.php
@@ -1,217 +1,72 @@
 <?php
 
-$to = "dylan.keys95@gmail.com";
-$subject = "jdkeysfitness | Booking confirmation";
+// If necessary, modify the path in the require statement below to refer to the 
+// location of your Composer autoload.php file.
+require 'vendor/autoload.php';
+require 'config.php';
 
-$message = '<!DOCTYPE html>
-<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
-<head>
- <meta charset="UTF-8" />
- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- <!--[if !mso]><!-- -->
- <meta http-equiv="X-UA-Compatible" content="IE=edge" />
- <!--<![endif]-->
- <meta name="viewport" content="width=device-width, initial-scale=1.0" />
- <meta name="format-detection" content="telephone=no, date=no, address=no, email=no" />
- <meta name="x-apple-disable-message-reformatting" />
- <link href="https://fonts.googleapis.com/css?family=Fira+Sans:ital,wght@0,300;0,400;0,500" rel="stylesheet" />
- <title>Untitled</title>
- <!-- Made with Postcards Email Builder by Designmodo -->
- <!--[if !mso]><!-- -->
- <style>
- @font-face { font-family: "Fira Sans"; font-style: normal; font-weight: 300; font-display: swap; src: local("Fira Sans Light"), local("FiraSans-Light"), url(https://fonts.gstatic.com/s/firasans/v10/va9B4kDNxMZdWfMOD5VnPKreSxf6Xl7Gl3LX.woff2) format("woff2"); unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F; }
-         @font-face { font-family: "Fira Sans"; font-style: normal; font-weight: 300; font-display: swap; src: local("Fira Sans Light"), local("FiraSans-Light"), url(https://fonts.gstatic.com/s/firasans/v10/va9B4kDNxMZdWfMOD5VnPKreQhf6Xl7Gl3LX.woff2) format("woff2"); unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116; }
-         @font-face { font-family: "Fira Sans"; font-style: normal; font-weight: 300; font-display: swap; src: local("Fira Sans Light"), local("FiraSans-Light"), url(https://fonts.gstatic.com/s/firasans/v10/va9B4kDNxMZdWfMOD5VnPKreSBf6Xl7Gl3LX.woff2) format("woff2"); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF; }
-         @font-face { font-family: "Fira Sans"; font-style: normal; font-weight: 300; font-display: swap; src: local("Fira Sans Light"), local("FiraSans-Light"), url(https://fonts.gstatic.com/s/firasans/v10/va9B4kDNxMZdWfMOD5VnPKreRhf6Xl7Glw.woff2) format("woff2"); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }
- </style>
- <!--<![endif]-->
- <style>
- html, body { margin: 0 !important; padding: 0 !important; min-height: 100% !important; width: 100% !important; -webkit-font-smoothing: antialiased; }
-         * { -ms-text-size-adjust: 100%; }
-         #outlook a { padding: 0; }
-         .ReadMsgBody, .ExternalClass { width: 100%; }
-         .ExternalClass, .ExternalClass p, .ExternalClass td, .ExternalClass div, .ExternalClass span, .ExternalClass font { line-height: 100%; }
-         table, td, th { mso-table-lspace: 0 !important; mso-table-rspace: 0 !important; border-collapse: collapse; }
-         u + .body table, u + .body td, u + .body th { will-change: transform; }
-         body, td, th, p, div, li, a, span { -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; mso-line-height-rule: exactly; }
-         img { border: 0; outline: 0; line-height: 100%; text-decoration: none; -ms-interpolation-mode: bicubic; }
-         a[x-apple-data-detectors] { color: inherit !important; text-decoration: none !important; }
-         .body .pc-project-body { background-color: transparent !important; }
-                 
- 
-         @media (min-width: 621px) {
-             .pc-lg-hide {  display: none; } 
-             .pc-lg-bg-img-hide { background-image: none !important; }
-         }
- </style>
- <style>
- @media (max-width: 620px) {
- .pc-project-body {min-width: 0px !important;}
- .pc-project-container {width: 100% !important;}
- .pc-sm-hide, .pc-w620-gridCollapsed-1 > tbody > tr > .pc-sm-hide {display: none !important;}
- .pc-sm-bg-img-hide {background-image: none !important;}
- .pc-w620-padding-25-35-0-35 {padding: 25px 35px 0px 35px !important;}
- .pc-w620-font-size-30px {font-size: 30px !important;}
- .pc-w620-line-height-133pc {line-height: 133% !important;}
- .pc-w620-padding-35-35-35-35 {padding: 35px 35px 35px 35px !important;}
- 
- .pc-w620-gridCollapsed-1 > tbody,.pc-w620-gridCollapsed-1 > tbody > tr,.pc-w620-gridCollapsed-1 > tr {display: inline-block !important;}
- .pc-w620-gridCollapsed-1.pc-width-fill > tbody,.pc-w620-gridCollapsed-1.pc-width-fill > tbody > tr,.pc-w620-gridCollapsed-1.pc-width-fill > tr {width: 100% !important;}
- .pc-w620-gridCollapsed-1.pc-w620-width-fill > tbody,.pc-w620-gridCollapsed-1.pc-w620-width-fill > tbody > tr,.pc-w620-gridCollapsed-1.pc-w620-width-fill > tr {width: 100% !important;}
- .pc-w620-gridCollapsed-1 > tbody > tr > td,.pc-w620-gridCollapsed-1 > tr > td {display: block !important;width: auto !important;padding-left: 0 !important;padding-right: 0 !important;margin-left: 0 !important;}
- .pc-w620-gridCollapsed-1.pc-width-fill > tbody > tr > td,.pc-w620-gridCollapsed-1.pc-width-fill > tr > td {width: 100% !important;}
- .pc-w620-gridCollapsed-1.pc-w620-width-fill > tbody > tr > td,.pc-w620-gridCollapsed-1.pc-w620-width-fill > tr > td {width: 100% !important;}
- .pc-w620-gridCollapsed-1 > tbody > .pc-grid-tr-first > .pc-grid-td-first,.pc-w620-gridCollapsed-1 > .pc-grid-tr-first > .pc-grid-td-first {padding-top: 0 !important;}
- .pc-w620-gridCollapsed-1 > tbody > .pc-grid-tr-last > .pc-grid-td-last,.pc-w620-gridCollapsed-1 > .pc-grid-tr-last > .pc-grid-td-last {padding-bottom: 0 !important;}
- 
- .pc-w620-gridCollapsed-0 > tbody > .pc-grid-tr-first > td,.pc-w620-gridCollapsed-0 > .pc-grid-tr-first > td {padding-top: 0 !important;}
- .pc-w620-gridCollapsed-0 > tbody > .pc-grid-tr-last > td,.pc-w620-gridCollapsed-0 > .pc-grid-tr-last > td {padding-bottom: 0 !important;}
- .pc-w620-gridCollapsed-0 > tbody > tr > .pc-grid-td-first,.pc-w620-gridCollapsed-0 > tr > .pc-grid-td-first {padding-left: 0 !important;}
- .pc-w620-gridCollapsed-0 > tbody > tr > .pc-grid-td-last,.pc-w620-gridCollapsed-0 > tr > .pc-grid-td-last {padding-right: 0 !important;}
- 
- .pc-w620-tableCollapsed-1 > tbody,.pc-w620-tableCollapsed-1 > tbody > tr,.pc-w620-tableCollapsed-1 > tr {display: block !important;}
- .pc-w620-tableCollapsed-1.pc-width-fill > tbody,.pc-w620-tableCollapsed-1.pc-width-fill > tbody > tr,.pc-w620-tableCollapsed-1.pc-width-fill > tr {width: 100% !important;}
- .pc-w620-tableCollapsed-1.pc-w620-width-fill > tbody,.pc-w620-tableCollapsed-1.pc-w620-width-fill > tbody > tr,.pc-w620-tableCollapsed-1.pc-w620-width-fill > tr {width: 100% !important;}
- .pc-w620-tableCollapsed-1 > tbody > tr > td,.pc-w620-tableCollapsed-1 > tr > td {display: block !important;width: auto !important;}
- .pc-w620-tableCollapsed-1.pc-width-fill > tbody > tr > td,.pc-w620-tableCollapsed-1.pc-width-fill > tr > td {width: 100% !important;box-sizing: border-box !important;}
- .pc-w620-tableCollapsed-1.pc-w620-width-fill > tbody > tr > td,.pc-w620-tableCollapsed-1.pc-w620-width-fill > tr > td {width: 100% !important;box-sizing: border-box !important;}
- }
- @media (max-width: 520px) {
- .pc-w520-padding-25-30-0-30 {padding: 25px 30px 0px 30px !important;}
- .pc-w520-padding-30-30-30-30 {padding: 30px 30px 30px 30px !important;}
- }
- </style>
- <!--[if !mso]><!-- -->
- <style>
- @font-face { font-family: "Fira Sans"; font-style: normal; font-weight: 400; src: url("https://fonts.gstatic.com/s/firasans/v17/va9E4kDNxMZdWfMOD5VvmYjN.woff") format("woff"), url("https://fonts.gstatic.com/s/firasans/v17/va9E4kDNxMZdWfMOD5VvmYjL.woff2") format("woff2"); } @font-face { font-family: "Fira Sans"; font-style: normal; font-weight: 500; src: url("https://fonts.gstatic.com/s/firasans/v17/va9B4kDNxMZdWfMOD5VnZKveSBf8.woff") format("woff"), url("https://fonts.gstatic.com/s/firasans/v17/va9B4kDNxMZdWfMOD5VnZKveSBf6.woff2") format("woff2"); } @font-face { font-family: "Fira Sans"; font-style: normal; font-weight: 300; src: url("https://fonts.gstatic.com/s/firasans/v17/va9B4kDNxMZdWfMOD5VnPKreSBf8.woff") format("woff"), url("https://fonts.gstatic.com/s/firasans/v17/va9B4kDNxMZdWfMOD5VnPKreSBf6.woff2") format("woff2"); }
- </style>
- <!--<![endif]-->
- <!--[if mso]>
-    <style type="text/css">
-        .pc-font-alt {
-            font-family: Arial, Helvetica, sans-serif !important;
-        }
-    </style>
-    <![endif]-->
- <!--[if gte mso 9]>
-    <xml>
-        <o:OfficeDocumentSettings>
-            <o:AllowPNG/>
-            <o:PixelsPerInch>96</o:PixelsPerInch>
-        </o:OfficeDocumentSettings>
-    </xml>
-    <![endif]-->
-</head>
+use Aws\Ses\SesClient;
+use Aws\Exception\AwsException;
 
-<body class="body pc-font-alt" style="width: 100% !important; min-height: 100% !important; margin: 0 !important; padding: 0 !important; line-height: 1.5; font-weight: normal; color: #2D3A41; mso-line-height-rule: exactly; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-variant-ligatures: normal; text-rendering: optimizeLegibility; -moz-osx-font-smoothing: grayscale; background-color: #f4f4f4;" bgcolor="#f4f4f4">
- <table class="pc-project-body" style="table-layout: fixed; width: 100%; min-width: 600px; background-color: #f4f4f4;" bgcolor="#f4f4f4" border="0" cellspacing="0" cellpadding="0" role="presentation">
-  <tr>
-   <td align="center" valign="top">
-    <table class="pc-project-container" align="center" style="width: 600px; max-width: 600px;" border="0" cellpadding="0" cellspacing="0" role="presentation">
-     <tr>
-      <td style="padding: 20px 0px 20px 0px;" align="left" valign="top">
-       <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
-        <tr>
-         <td valign="top">
-          <table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation">
-           <tr>
-            <td valign="top" class="pc-w520-padding-25-30-0-30 pc-w620-padding-25-35-0-35" style="padding: 25px 40px 0px 40px; height: unset; background-color: #ffffff;" bgcolor="#ffffff">
-             <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
-              <tr>
-               <td valign="top" align="left">
-                <div class="pc-font-alt" style="text-decoration: none;">
-                 <div style="font-size: 32px;line-height: 42px;text-align:left;text-align-last:left;color:#d62020;letter-spacing:-0.2px;font-weight:400;font-style:normal;">
-                  <div><span style="font-family: "Helvetica", Arial, serif;">jdkeys</span><span style="font-family: "Helvetica", Arial, serif; color: rgb(0, 0, 0);">fitness</span>
-                  </div>
-                 </div>
-                </div>
-               </td>
-              </tr>
-             </table>
-            </td>
-           </tr>
-          </table>
-         </td>
-        </tr>
-        <tr>
-         <td valign="top">
-          <table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation">
-           <tr>
-            <td valign="top" class="pc-w520-padding-30-30-30-30 pc-w620-padding-35-35-35-35" style="padding: 40px 40px 40px 40px; height: unset; background-color: #000000;" bgcolor="#000000">
-             <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
-              <tr>
-               <td align="center" valign="top" style="padding: 0px 0px 30px 0px;">
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%" style="margin-right: auto; margin-left: auto;">
-                 <tr>
-                  <td valign="top" align="center">
-                   <div class="pc-font-alt" style="text-decoration: none;">
-                    <div style="font-size: 30px;line-height: 128%;text-align:center;text-align-last:center;color:#ffffff;letter-spacing:-0.6px;font-weight:700;font-style:normal;">
-                     <div><span style="font-family: "Helvetica", Arial, serif; font-size: 36px; line-height: 128%;" class="pc-w620-font-size-30px pc-w620-line-height-133pc">Your session is now booked!</span>
-                     </div>
-                    </div>
-                   </div>
-                  </td>
-                 </tr>
-                </table>
-               </td>
-              </tr>
-             </table>
-             <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
-              <tr>
-               <td align="center" valign="top" style="padding: 0px 0px 29px 0px;">
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%" style="margin-right: auto; margin-left: auto;">
-                 <tr>
-                  <td valign="top" align="center">
-                   <div class="pc-font-alt" style="text-decoration: none;">
-                    <div style="font-size: 18px;line-height: 156%;text-align:center;text-align-last:center;color:#ffffff;letter-spacing:-0.2px;font-weight:300;font-style:normal;">
-                     <div><span style="font-family: "Fira Sans", Arial, Helvetica, sans-serif;line-height: 156%;">I look forward to seeing you soon!</span>
-                     </div>
-                     <div style="line-height:156%;font-size:18px;">
-                      <br>
-                     </div>
-                     <div><span style="font-family: "Fira Sans", Arial, Helvetica, sans-serif;line-height: 156%;">Date/time: X</span>
-                     </div>
-                     <div><span style="font-family: "Fira Sans", Arial, Helvetica, sans-serif;line-height: 156%;">Location: X</span>
-                     </div>
-                    </div>
-                   </div>
-                  </td>
-                 </tr>
-                </table>
-               </td>
-              </tr>
-             </table>
-            </td>
-           </tr>
-          </table>
-         </td>
-        </tr>
-        <tr>
-         <td>
-          <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
-           <tr>
-            <td align="center" valign="top" style="padding-top: 20px; padding-bottom: 20px; vertical-align: top;">
-             <a href="https://postcards.email/?uid=MjkyNjQ2&type=footer" target="_blank" style="text-decoration: none; overflow: hidden; border-radius: 2px; display: inline-block;">
-              <img src="https://cloudfilesdm.com/postcards/promo-footer-dark.jpg" width="198" height="46" alt="Made with (o -) postcards" style="width: 198px; height: auto; margin: 0 auto; border: 0; outline: 0; line-height: 100%; -ms-interpolation-mode: bicubic; vertical-align: top;">
-             </a>
-             <img src="https://api-postcards.designmodo.com/tracking/mail/promo?uid=MjkyNjQ2" width="1" height="1" alt="" style="display:none; width: 1px; height: 1px;">
-            </td>
-           </tr>
-          </table>
-         </td>
-        </tr>
-       </table>
-      </td>
-     </tr>
-    </table>
-   </td>
-  </tr>
- </table>
-</body>
-</html>';
+$credentials = new Aws\Credentials\Credentials($CFG->aws_key, $CFG->aws_secret);
 
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= 'From: <webmaster@dylankeys.com>' . "\r\n";
+// Create an SesClient. Change the value of the region parameter if you're 
+// using an AWS Region other than US West (Oregon). Change the value of the
+// profile parameter if you want to use a profile in your credentials file
+// other than the default.
+$SesClient = new SesClient([
+    'credentials' => $credentials,
+    'region'  => 'eu-west-1'
+]);
 
-mail($to,$subject,$message,$headers);
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender_email = 'bookings@jdkeysfitness.com';
+
+// Replace these sample addresses with the addresses of your recipients. If
+// your account is still in the sandbox, these addresses must be verified.
+$recipient_emails = ['dylankeys@btinternet.com'];
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment the following variable, and the
+// 'ConfigurationSetName' => $configuration_set argument below.
+$configuration_set = 'my-first-configuration-set';
+
+$subject = 'Amazon SES test (AWS SDK for PHP)';
+$html_body =  '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+              '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+              'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+              'AWS SDK for PHP</a>.</p>';
+$char_set = 'UTF-8';
+
+try {
+    $result = $SesClient->sendEmail([
+        'Destination' => [
+            'ToAddresses' => $recipient_emails,
+        ],
+        'ReplyToAddresses' => [$sender_email],
+        'Source' => $sender_email,
+        'Message' => [
+          'Body' => [
+              'Html' => [
+                  'Charset' => $char_set,
+                  'Data' => $html_body,
+              ],
+          ],
+          'Subject' => [
+              'Charset' => $char_set,
+              'Data' => $subject,
+          ],
+        ],
+        // If you aren't using a configuration set, comment or delete the
+        // following line
+        'ConfigurationSetName' => $configuration_set,
+    ]);
+    $messageId = $result['MessageId'];
+    echo("Email sent! Message ID: $messageId"."\n");
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+    echo "\n";
+}
